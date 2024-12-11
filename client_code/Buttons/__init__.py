d09162fbd15d1501e49992e582d235bb65eacc69
jarahybrant@gmail.com <jarahybrant@gmail.com>
2024-12-11 19:02:00 +0000

    Created Images/css3-logo.png, W3/btns.css, W3/JS.html and Images/html5-logo.png; Edited theme.css, standard-page.html, Form2 and Buttons; Deleted Images/css_919826.png, Images/html-5_5968267.png and W3/btn.css

--- client_code/Buttons/__init__.py
+++ client_code/Buttons/__init__.py
@@ -1,11 +1,20 @@
 import anvil.server
 from bs4 import BeautifulSoup as BS4
+import re
 
-with open("_/theme/standard-page.html") as html:
-  soup = BS4(html.read(), 'html.parser')
-  tags = soup()
-  
+# Open and parse the HTML file
+with open("_/theme/W3/JS.html", "r") as html_file:
+    soup = BS4(html_file.read(), 'html.parser')
 
-
-
+# Find all <script> tags
+script_tags = soup.find_all('script')
+# Extract and print JavaScript line by line
+def displayJS():
+for script in script_tags:
+    if script.string:  # Ensure the <script> tag contains text
+        js_code = script.string.strip()
+        js_lines = re.split(r'\n', js_code)  # Split code into lines using regex
+        for line in js_lines:
+            print(line.strip())  # Print each line after stripping extra whitespace
+            
 
--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -3,14 +3,21 @@ from anvil import *
 import anvil.server
 
 
+
 class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    # Assign CSS classes to buttons
   
     # Any code you write here will run before the form opens.
+  btns = []
+  css3_btn = 
+  html5_btn = 
   
-  
+  def btnbkg(self):
+    
+    
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
